5438ed4d22bcb919c588f73f691c024959eb8bc9
Spencer Paciello <scobydoo523@aol.com>
2017-09-17 12:43:06 -0400

    k

--- cd/asciiLetters.js
+++ cd/asciiLetters.js
@@ -1,1 +1,161 @@
 // JavaScript Document
+function getAsciiLetters(letter){
+		if (letter == "A"){
+			return[0,1,0,0,0,0,0,1]
+		}
+		if (letter == "B"){
+			return[0,1,0,0,0,0,1,0]
+		}
+		if (letter == "C"){
+			return[0,1,0,0,0,0,1,1]
+		}
+		if (letter == "D"){
+			return[0,1,0,0,0,1,0,0]
+		}
+		if (letter == "E"){
+			return[0,1,0,0,0,1,0,1]
+		}
+		if (letter == "F"){
+			return[0,1,0,0,0,1,1,0]
+		}
+		if (letter == "G"){
+			return[0,1,0,0,0,1,1,1]
+		}
+		if (letter == "H"){
+			return[0,1,0,0,1,0,0,0]
+		}
+		if (letter == "I"){
+			return[0,1,0,0,1,0,0,1]
+		}
+		if (letter == "J"){
+			return[0,1,0,0,1,0,1,0]
+		}
+		if (letter == "K"){
+			return[0,1,0,0,1,0,1,1]
+		}
+		if (letter == "L"){
+			return[0,1,0,0,1,1,0,0]
+		}
+		if (letter == "M"){
+			return[0,1,0,0,1,1,0,1]
+		}
+		if (letter == "N"){
+			return[0,1,0,0,1,1,1,0]
+		}
+		if (letter == "O"){
+			return[0,1,0,0,1,1,1,1]
+		}
+		if (letter == "P"){
+			return[0,1,0,1,0,0,0,0]
+		}
+		if (letter == "Q"){
+			return[0,1,0,1,0,0,0,1]
+		}
+		if (letter == "R"){
+			return[0,1,0,1,0,0,1,0]
+		}
+		if (letter == "S"){
+			return[0,1,0,1,0,0,1,1]
+		}
+		if (letter == "T"){
+			return[0,1,0,1,0,1,0,0]
+		}
+		if (letter == "U"){
+			return[0,1,0,1,0,1,0,1]
+		}
+		if (letter == "V"){
+			return[0,1,0,1,0,1,1,0]
+		}
+		if (letter == "W"){
+			return[0,1,0,1,0,1,1,1]
+		}
+		if (letter == "X"){
+			return[0,1,0,1,1,0,0,0]
+		}
+		if (letter == "Y"){
+			return[0,1,0,1,1,0,0,1]
+		}
+		if (letter == "Z"){
+			return[0,1,0,1,1,0,1,0]
+		}
+		if (letter == "a"){
+			return [0,1,1,0,0,0,0,1]
+		}
+		if (letter == "b"){
+			return [0,1,1,0,0,0,1,0]
+		}
+		if (letter == "c"){
+			return [0,1,1,0,0,0,1,1]
+		}
+		if (letter == "d"){
+			return [0,1,1,0,0,1,0,0]
+		}
+		if (letter == "e"){
+			return [0,1,1,0,0,1,0,1]
+		}
+		if (letter == "f"){
+			return [0,1,1,0,0,1,1,0]
+		}
+		if (letter == "g"){
+			return [0,1,1,0,0,1,1,1]
+		}
+		if (letter == "h"){
+			return [0,1,1,0,1,0,0,0]
+		}
+		if (letter == "i"){
+			return [0,1,1,0,1,0,0,1]
+		}
+		if (letter == "j"){
+			return [0,1,1,0,1,0,1,0]
+		}
+		if (letter == "k"){
+			return [0,1,1,0,1,0,1,1]
+		}
+		if (letter == "l"){
+			return[0,1,1,0,1,1,0,0]
+		}
+		if (letter == "m"){
+			return[0,1,1,0,1,1,0,1]
+		}
+		if (letter == "n"){
+			return[0,1,1,0,1,1,1,0]
+		}
+		if (letter == "o"){
+			return[0,1,1,0,1,1,1,1]
+		}
+		if (letter == "p"){
+			return[0,1,1,1,0,0,0,0]
+		}
+		if (letter == "q"){
+			return[0,1,1,1,0,0,0,1]
+		}
+		if (letter == "r"){
+			return[0,1,1,1,0,0,1,0]
+		}
+		if (letter == "s"){
+			return[0,1,1,1,0,0,1,1]
+		}
+		if (letter == "t"){
+			return[0,1,1,1,0,1,0,0]
+		}
+		if (letter == "u"){
+			return[0,1,1,1,0,1,0,1]
+		}
+		if (letter == "v"){
+			return[0,1,1,1,0,1,1,0]
+		}
+		if (letter == "w"){
+			return[0,1,1,1,0,1,1,1]
+		}
+		if (letter == "x"){
+			return[0,1,1,1,1,0,0,0]
+		}
+		if (letter == "y"){
+			return[0,1,1,1,1,0,0,1]
+		}
+		if (letter == "z"){
+			return[0,1,1,1,1,0,1,0]
+		}
+		
+		
+	}
